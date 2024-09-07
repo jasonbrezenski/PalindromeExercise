a95df379eced02e1a97e0de01912ad8de4c1ea64
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-09-07 11:41:22 -0500

    Palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string word)
+    {
+        var reversedWord = "";
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reversedWord += word[i];
+        }
+
+        if (reversedWord.ToLower() == word.ToLower())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        //throw new NotImplementedException();
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
+using System.Diagnostics.Contracts;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("Mom", true)]
+        [InlineData("Friday", false)]
+        [InlineData("Noon", true)]
+        [InlineData("Huskers", false)]
+        
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            //Arrange
+            var wordSmith = new WordSmith();
+            
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+            
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
